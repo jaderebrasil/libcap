4d5b872db3fae04954bc2336391c308dc24fb294
Jader Brasil <jaderebrasil@gmail.com>
2021-10-12 14:21:40 -0300

    cap cm for compress map files

--- cap/Program.cs
+++ cap/Program.cs
@@ -1,26 +1,57 @@
 ﻿using System;
+using System.IO;
 using LibCap;
+using System.Linq;
 
 namespace cap
 {
     class Program
     {
         private const string DG_TOCOCUMBA = "/home/jader/projs/dotnet/cap/LibCap.Tests/test_assets/json/Dungeon_tococumba.json";
+        
+        static void WriteUsage() {
+            Console.WriteLine("usage: cap cm output.cap map1.json map2.json <...>");
+            Console.WriteLine("Options:");
+            Console.WriteLine("\tcm - compress map");
+        }
         static void Main(string[] args)
         {
+            CapError res;
             var builder = new CapBuilder();
             
-            var res = builder.AddAsset(DG_TOCOCUMBA, AssetType.MAP);
-
-            if (!res.IsOk) {
-                Console.WriteLine(res.Msg);
-                return;
+            if (args.Length <= 2) {
+                WriteUsage();
+                Environment.Exit(1);
             }
 
-            if (builder.ExportCap("DungeonTococumba.zip", true))
-                Console.WriteLine("Done.");
-            else
-                Console.WriteLine("Dependency problem.");
+            switch (args[0]) {
+                case "cm":
+                    foreach (var file in args.Skip(2)) {
+                        if (!File.Exists(file)) {
+                            Console.WriteLine("File {0} not found.", file);
+                            Environment.Exit(1);
+                        }
+                    
+                        res = builder.AddAsset(file, AssetType.MAP);
+                        if (!res.IsOk) {
+                            Console.WriteLine(res.Msg);
+                            Environment.Exit(1);
+                        }
+                    }
+
+                    if (builder.ExportCap(args[1], true))
+                        Console.WriteLine("Done.");
+                    else
+                        Console.WriteLine("Something went wrong.");
+
+                    break;
+                
+                default:
+                    WriteUsage();
+                    Environment.Exit(1);
+                    break;
+            }
+
         }
     }
 }
